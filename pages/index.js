1812dbb49dd57a284ab97d96aac46bb9935d674d
souravrane <sourav1121997@gmail.com>
2021-06-23 17:22:56 +0530

    raw coins created

--- pages/index.js
+++ pages/index.js
@@ -1,8 +1,7 @@
 import Head from "next/head";
-import Image from "next/image";
 import SearchBar from "../components/SearchBar";
-
-export default function Home() {
+import CoinList from "../components/CoinList";
+export default function Home({ filteredCoins }) {
   return (
     <div>
       <Head>
@@ -12,6 +11,21 @@ export default function Home() {
       </Head>
 
       <SearchBar type="text" placeholder="Search" />
+      <CoinList filteredCoins={filteredCoins} />
     </div>
   );
 }
+
+export const getServerSideProps = async () => {
+  const res = await fetch(
+    "https://api.coingecko.com/api/v3/coins/markets?vs_currency=usd&order=market_cap_desc&per_page=10&page=1&sparkline=false"
+  );
+
+  const filteredCoins = await res.json();
+
+  return {
+    props: {
+      filteredCoins,
+    },
+  };
+};
